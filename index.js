2a576814a6af5648205f7ff1e10296d6dcec0998
Davidson Fellipe <email@fellipe.com>
2016-02-07 02:14:54 -0500

    Starting support test multiplugins

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var engine = require('./lib/engine');
 
 var tests = {
   plugins: [
-    'keepfast-contrib-phantomas'
+    'keepfast-contrib-phantomas',
+    'keepfast-contrib-psi'
   ],
   profiles: []
 };
 
-engine.runURL(tests, 'http://webcomponentsweekly.me/');
+engine.runURL(tests, 'http://fellipe.com/');
+// engine.runURL(tests, 'https://loadsmart.com');
+engine.runURL(tests, 'http://globoesporte.globo.com/');
+// engine.runURL(tests, 'http://www.nytimes.com/');
+// engine.runURL(tests, 'https://www.google.com/');
+// engine.runURL(tests, 'https://github.com/');
