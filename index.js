52da14f680b8b138d63c21e1ccc104fbf32351a3
Davidson Fellipe <email@fellipe.com>
2016-02-08 01:39:22 -0500

    Improve way to compare values based in a strict condition

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var engine = require('./lib/engine');
 
 var tests = {
   plugins: [
-    'keepfast-contrib-phantomas',
+    'keepfast-contrib-yslow',
     'keepfast-contrib-psi',
-    'keepfast-contrib-yslow'
+    'keepfast-contrib-phantomas'
   ],
   profiles: []
 };
 
-// engine.runURL(tests, 'http://fellipe.com/');
-engine.runURL(tests, 'https://loadsmart.com');
+engine.runURL(tests, 'http://fellipe.com/');
+// engine.runURL(tests, 'https://loadsmart.com');
+// engine.runURL(tests, 'https://github.com/');
 // engine.runURL(tests, 'http://globoesporte.globo.com/');
 // engine.runURL(tests, 'http://julianamalta.com/');
 // engine.runURL(tests, 'http://www.nytimes.com/');
 // engine.runURL(tests, 'https://www.google.com/');
-// engine.runURL(tests, 'https://github.com/');
